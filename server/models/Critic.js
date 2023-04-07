91d6b0c78aaf847ecdbb05e58760e561f8cd7242
victorlmorales <victor.l.morales2020@gmail.com>
2023-04-06 21:43:19 -0500

    Fixed the signup page bug

--- server/models/Critic.js
+++ server/models/Critic.js
@@ -1,12 +1,43 @@
 const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
 const criticSchema = new Schema({
-  name: {
+  CriticText: {
     type: String,
-    required: true,
-    unique: true,
+    required: 'You need to leave a Critic!',
+    minlength: 1,
+    maxlength: 280,
     trim: true,
   },
+  CriticAuthor: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => dateFormat(timestamp),
+  },
+  comments: [
+    {
+      commentText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280,
+      },
+      commentAuthor: {
+        type: String,
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dateFormat(timestamp),
+      },
+    },
+  ],
 });
 
 const Critic = model('Critic', criticSchema);
