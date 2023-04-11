20150ae5df7a231b9e23b3b5270134e53b07362b
sdfields <spencerdfields@gmail.com>
2023-04-11 14:39:05 -0500

    updated models need to decide on donations model or not

--- server/models/Movie.js
+++ server/models/Movie.js
@@ -1,11 +1,22 @@
 const { Schema, model } = require('mongoose');
 
 const movieSchema = new Schema({
+  omdbID: {
+    type: String,
+    required: true
+  },
   name: {
     type: String,
     required: true,
     unique: true,
   },
+  // Possibly match model items to imdb info pulled for movies page?
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Review',
+    },
+  ],
 });
 
 const Movie = model('Movie', movieSchema);
