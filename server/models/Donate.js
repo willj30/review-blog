e9f354851e43e63781b6198305e1d8fbb4624830
Call Carl <callienicole0419@gmail.com>
2023-04-11 21:17:47 -0500

    added stripe in package.json, imported to resolvers, added donate model

---
+++ server/models/Donate.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const donationSchema = new Schema({
+    purchaseDate: {
+        type: Date,
+        default: Date.now
+    },
+    products: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'product'
+        }
+    ]
+});
+const Donate = mongoose.model('donation', donationSchema);
+
+module.exports = Donate;
