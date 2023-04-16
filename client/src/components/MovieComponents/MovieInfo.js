af55563ec31d6a59a5acb26c538bcea5c8960221
victorlmorales <victor.l.morales2020@gmail.com>
2023-04-16 17:42:44 -0500

    Added Review form to Movie Page and took out Review Form out of profile page

--- client/src/components/MovieComponents/MovieInfo.js
+++ client/src/components/MovieComponents/MovieInfo.js
@@ -13,7 +13,6 @@ function MovieInfo(props) {
       <h3>Genre: {props.genre}</h3>
       <h3>Released: {props.released}</h3>
       <h3>imdbRating: {props.imdbRating}</h3>
-        <h3>imdbID: {props.imdbID}</h3>
     </div>
   );
 }
