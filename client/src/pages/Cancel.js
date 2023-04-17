5c4743f38230296cbe9ae13e5eb347409e657394
Call Carl <callienicole0419@gmail.com>
2023-04-17 11:28:58 -0500

    added success and cancel pages for stripe payments

---
+++ client/src/pages/Cancel.js
@@ -0,0 +1,13 @@
+import  '../App';
+
+function Cancel() {
+    return (
+        <div className='Cancel'>
+            <header className='Cancel-header'>
+                <h1>Payment cancelled successfully.</h1>
+            </header>
+        </div>
+    );
+}
+
+export default Cancel;
