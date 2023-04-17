8ceb1c9162eeb287e5e9e95af176ae7d31c4a114
willj30 <willjaramillo30@gmail.com>
2023-04-17 14:05:08 -0500

    Updated homepage movies and moviepage structure

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,7 +6,10 @@ import ReviewList from '../components/ReviewList';
 
 import { QUERY_REVIEWS } from '../utils/queries';
 import SingleMovie from '../components/MovieComponents/SingleMovie';
-
+import SecondMovie from '../components/MovieComponents/SecondMovie';
+import ThirdMovie from '../components/MovieComponents/ThirdMovie';
+import FourthMovie from '../components/MovieComponents/FourthMovie';
+import FifthMovie from '../components/MovieComponents/FifthMovie';
 
 const Home = () => {
   const { loading, data } = useQuery(QUERY_REVIEWS);
@@ -16,9 +19,27 @@ const Home = () => {
     <main>
       
       <div className=" flex-row justify-center">
+        <div className="posters img-poster">
         <div><SingleMovie>
           
           </SingleMovie>
+        </div>
+        <div><SecondMovie>
+          
+          </SecondMovie>
+        </div>
+        <div><ThirdMovie>
+          
+          </ThirdMovie>
+        </div>
+        <div><FourthMovie>
+          
+          </FourthMovie>
+        </div>
+        <div><FifthMovie>
+          
+          </FifthMovie>
+        </div>
         </div>
         <div
           className=" col-12 col-md-10 mb-3 p-3"
